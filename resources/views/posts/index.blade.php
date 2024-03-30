2a10b5ee3483857453b564b30a944bf202c6c6e1
chisato <peas.andps@gamil.com>
2024-03-30 18:17:52 +0900

    laravel-basic-kadai03

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,8 +4,30 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <style>
+    table, table th, table td{
+      border: 1px solid black;
+      border-collapse: collapse;
+    }
+  </style>
 </head>
 <body>
-  <h1>投稿一覧</h1>
+  <h1>ぽすと</h1>
+  <table>
+    <tr>
+      <th>タイトル</th>
+      <th>本文</th>
+      <th>作成日時</th>
+      <th>更新日時</th>
+    </tr>
+    @foreach($posts as $post)
+      <tr>
+        <td>{{ $post->title }}</td>
+        <td>{{ $post->content }}</td>
+        <td>{{ $post->created_at }}</td>
+        <td>{{ $post->updated_at }}</td>
+      </tr>
+    @endforeach
+  </table>
 </body>
 </html>
